39673e05c70e56920fe81a963168bb1d904029a5
kiminzae <pumple3411@gmail.com>
2026-09-15 13:53:23 +0900

    fix: week2_first_app 폴더 삭제

--- week2_first_app/README.md
+++
@@ -1,3 +0,0 @@
-# week2_first_app
-
-A new Flutter project.
--- week2_first_app/android/app/src/main/kotlin/com/example/week2_first_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.week2_first_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
